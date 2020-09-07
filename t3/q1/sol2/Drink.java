f88e8f284b31b15c6c13583062deb781f3f0cd0b
Yi Li <yi_li@ntu.edu.sg>
2020-09-07 10:49:23 +0800

    Tabs to spaces. Proper indentation.

--- t3/q1/sol2/Drink.java
+++ t3/q1/sol2/Drink.java
@@ -1,11 +1,11 @@
 public class Drink {
-	private String name;
-	private double cost;
+  private String name;
+  private double cost;
 
-	public Drink(String n, double c) {
-		name = n;
-		cost = c;
-	}
-	public String getName() { return name;}
-	public double getCost() { return cost;}
+  public Drink(String n, double c) {
+    name = n;
+    cost = c;
+  }
+  public String getName() { return name;}
+  public double getCost() { return cost;}
 }
--- t3/q2/sol/Circle.java
+++ t3/q2/sol/Circle.java
@@ -1,21 +1,21 @@
 public class Circle extends Point {
-	private double radius;
+  private double radius;
 
-	public Circle() { radius = 1; }
-	public Circle(double radius) { this.radius = radius;}
-	// note the use of super
-	public Circle(double radius, int a, int b) {
-		super(a,b);
-		this.radius = radius;
-	}
+  public Circle() { radius = 1; }
+  public Circle(double radius) { this.radius = radius;}
+  // note the use of super
+  public Circle(double radius, int a, int b) {
+    super(a,b);
+    this.radius = radius;
+  }
 
-	public double getRadius() { return radius; }
-	public void setRadius(double radius) { this.radius = radius;}
+  public double getRadius() { return radius; }
+  public void setRadius(double radius) { this.radius = radius;}
 
-	public double area() {
-		return Math.PI * Math.pow(radius,2);
-	}
-	public String toString(){
+  public double area() {
+    return Math.PI * Math.pow(radius,2);
+  }
+  public String toString(){
     return "Circle of radius " + radius + " at point [" + x + "," + y + "]";
-	}
+  }
 }
