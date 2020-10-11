965163c8efe0b116cd01fcd77f204e66c4c4ca0c
Yi Li <yi_li@ntu.edu.sg>
2020-10-11 22:52:05 +0800

    Tutorial 6 q2.

---
+++ t6/q2/CheckingAccount.java
@@ -0,0 +1,18 @@
+public class CheckingAccount {
+
+	public double getBalance() {
+		return 100;  // any valid double
+	}
+
+	public void addInsuffientFundFee()  {
+  } // void
+
+  public void noteReturnedCheck(Check theCheck) {
+  }
+
+  public void addDebitTransaction(int checkNumber, double amount) {
+  }
+
+  public void storePhotoOfCheck(Check theCheck) {
+  }
+}
