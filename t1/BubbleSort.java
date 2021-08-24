4fe12e236fc141dfeb29052309bc4d37dba3c297
Yi Li <yi_li@ntu.edu.sg>
2021-08-24 13:37:47 +0800

    move close

--- t1/BubbleSort.java
+++ t1/BubbleSort.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 class BubbleSort {
-  
+
   public static void bubble(int a[], int n) {
     int i, j, t;
     for (i = n - 2; i >= 0; i--) {
@@ -14,26 +14,27 @@ class BubbleSort {
       }
     }
   }// end function.
-  
+
   public static void main(String args[]) {
     int n, i;
     int a[] = new int[100];
-    
+
     System.out.print("\n\n Enter number of Integer elements to be sorted: ");
     Scanner in = new Scanner(System.in);
     n = in.nextInt();
-    in.close();
 
     for (i = 0; i <= n - 1; i++) {
       System.out.print("\n\n Enter integer value for element no. " + (i + 1) + " : ");
       a[i] = in.nextInt();
     }
+    in.close();
+
     bubble(a, n);
-    
+
     System.out.print("\n\n Final sorted array is : ");
     for (i = 0; i <= n - 1; i++)
     System.out.print(a[i] + "   ");
-    
+
     System.out.println(); // optional
   } // end program.
 }
