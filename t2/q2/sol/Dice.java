8684be3202c1058ca769caab75133db70469b116
Yi Li <yi_li@ntu.edu.sg>
2021-08-30 21:57:57 +0800

    format dice

--- t2/q2/sol/Dice.java
+++ t2/q2/sol/Dice.java
@@ -1,16 +1,19 @@
 public class Dice {
-    private int valueOfDice=1;
-	
-    public Dice() {
-	setDiceValue();
-    }
-    public void setDiceValue() {
-	valueOfDice = (int)(Math.random()*6)+1;
-    }
-    public int getDiceValue() {
-	return valueOfDice;
-    }
-    public void printDiceValue() {
-	System.out.println("Current Value is "+valueOfDice);
-    }
+  private int valueOfDice = 1;
+
+  public Dice() {
+    setDiceValue();
+  }
+
+  public void setDiceValue() {
+    valueOfDice = (int) (Math.random() * 6) + 1;
+  }
+
+  public int getDiceValue() {
+    return valueOfDice;
+  }
+
+  public void printDiceValue() {
+    System.out.println("Current Value is " + valueOfDice);
+  }
 }
--- t2/q2/sol/DiceApp.java
+++ t2/q2/sol/DiceApp.java
@@ -1,27 +1,27 @@
 import java.util.Scanner;
 
 public class DiceApp {
-    public static void main(String[] args) {
-	String once, twice;
-	int total;
-	int rollone, rolltwo;
-	
-	Scanner sc = new Scanner(System.in);
-	System.out.println("Press <key> to roll the first dice");
-	// once = sc.next();
-	Dice d1 = new Dice();
-	// d1.setDiceValue();
-	rollone = d1.getDiceValue();
-	d1.printDiceValue();
-		
-	System.out.println("Press <key> to roll second dice");
-	// twice = sc.next();
-	Dice d2 = new Dice();
-	// d2.setDiceValue();
-	rolltwo = d2.getDiceValue();
-	d2.printDiceValue();
+  public static void main(String[] args) {
+    String once, twice;
+    int total;
+    int rollone, rolltwo;
 
-	total = rollone + rolltwo;
-	System.out.println("Your total number is: " + total);
-    }
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Press <key> to roll the first dice");
+    // once = sc.next();
+    Dice d1 = new Dice();
+    // d1.setDiceValue();
+    rollone = d1.getDiceValue();
+    d1.printDiceValue();
+
+    System.out.println("Press <key> to roll second dice");
+    // twice = sc.next();
+    Dice d2 = new Dice();
+    // d2.setDiceValue();
+    rolltwo = d2.getDiceValue();
+    d2.printDiceValue();
+
+    total = rollone + rolltwo;
+    System.out.println("Your total number is: " + total);
+  }
 }
