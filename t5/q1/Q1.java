cc2e9c0b757b2fc43406f44d1620c709191981c9
Yi Li <yi_li@ntu.edu.sg>
2021-10-03 17:08:16 +0800

    Print class info

--- t5/q1/Q1.java
+++ t5/q1/Q1.java
@@ -1,49 +1,74 @@
-class A {
+class ClassA {
   void print(int x, String y) {
+    System.out.print("ClassA: ");
     System.out.println(x + ", " + y);
   }
 }
 
-class B extends A {
+class ClassB extends ClassA {
   void print(int x) {
+    System.out.print("ClassB: ");
     System.out.println(x);
   }
 }
 
-class C extends A {
+class ClassC extends ClassA {
   void print(String x, String y) {
+    System.out.print("ClassC: ");
     System.out.println(x + ", " + y);
   }
 }
 
-class D extends C {
+class ClassD extends ClassC {
   void print(String a, String b) {
+    System.out.print("ClassD: ");
     System.out.println(a + ", " + b);
   }
 }
 
-class E extends C {
+class ClassE extends ClassC {
   void print(String x) {
+    System.out.print("ClassE: ");
     System.out.println(x);
   }
 }
 
-class F extends E {
+class ClassF extends ClassE {
   void print(int x) {
+    System.out.print("ClassF: ");
     System.out.println(x);
   }
 }
 
 class G extends E {
   void print(String x) {
+    System.out.println(getClass().getName());
     System.out.println(x);
   }
 }
 
 public class Q1 {
   public static void main(String[] args) {
-    A a = new C();
-    G g = (G) a;
-    g.print("hello", "there");
+    // Q1
+    ClassF z = new ClassF();
+
+    // Q1(a)
+    z.print(9);
+
+    // Q1(b)
+    z.print(2, "Cx2002");
+
+    // Q1(c)
+    z.print("Object");
+
+    // Q1(d)
+    z.print("OODP", "Java");
+
+    // Q1(e)
+    // z.print("OODP", 2002);
+
+    // A a = new C();
+    // G g = (G) a;
+    // g.print("hello", "there");
   }
 }
