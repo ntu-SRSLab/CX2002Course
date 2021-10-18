9a5e0e7bf6b4f2cc485d14428067a1ca021787e2
Yi Li <yi_li@ntu.edu.sg>
2021-10-18 09:25:43 +0800

    reformat t7 files

---
+++ t7/q1/Bank.java
@@ -0,0 +1,31 @@
+public class Bank {
+
+  private CheckingAccount account ;
+
+  public double cashCheck(Check theCheck) {
+    double amount = theCheck.getAmount();
+    double balance = account.getBalance() ;
+
+    // Important : in order for method to access account
+    // means account is an instance variable of CheckingAccount class
+    if (balance < amount) {
+	    account.addInsuffientFundFee() ;
+
+	    // note typo in diagram, have to follow diagram as it is common source for all
+	    account.noteReturnedCheck(theCheck);
+	    returnCheck(theCheck);
+
+	    return -1 ;
+    }
+    
+    int checkNumber = theCheck.getCheckNumber() ;
+
+    account.addDebitTransaction(checkNumber, amount);
+    account.storePhotoOfCheck(theCheck);
+
+    return amount;
+  }
+
+  private void returnCheck(Check theCheck) {
+  }
+}
---
+++ t7/q1/Check.java
@@ -0,0 +1,9 @@
+public class Check {
+  public double getAmount() {
+    return 100; // any valid double
+  }
+
+  public int getCheckNumber() {
+    return 1234; // any valid int
+  }
+}
