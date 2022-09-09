a87b9a47284479669c4474519da4a5f9329da51b
Yi Li <yi_li@ntu.edu.sg>
2022-09-09 16:17:01 +0800

    untabify

--- t2/q1/sol/CircleApp.java
+++ t2/q1/sol/CircleApp.java
@@ -8,28 +8,28 @@ public class CircleApp
     double radius = 0.0;
     Scanner sc = new Scanner(System.in);
     do {
-	    System.out.println("==== Circle Computation =====");
-	    System.out.println("|1. Create a new circle     |");
-	    System.out.println("|2. Print Area              |");
-	    System.out.println("|3. Print circumference     |");
-	    System.out.println("|4. Quit                    |");
-	    System.out.println("=============================");
-	    System.out.println("Choose option (1-3):");
-	    option = sc.nextInt();
-	    if (option == 1) {
+      System.out.println("==== Circle Computation =====");
+      System.out.println("|1. Create a new circle     |");
+      System.out.println("|2. Print Area              |");
+      System.out.println("|3. Print circumference     |");
+      System.out.println("|4. Quit                    |");
+      System.out.println("=============================");
+      System.out.println("Choose option (1-3):");
+      option = sc.nextInt();
+      if (option == 1) {
         System.out.println("Enter the radius to compute the area and circumference");
         radius = sc.nextDouble();
         cir = new Circle(radius);
         System.out.println("A new circle is created");
-	    }
-	    else if (option == 2) {
+      }
+      else if (option == 2) {
         if (cir != null)
           cir.printArea();
-	    }
-	    else if (option == 3) {
+      }
+      else if (option == 3) {
         if (cir != null)
           cir.printCircumference();
-	    }
+      }
     } while (option < 4);
     System.out.println("Thank you!!");
   }
