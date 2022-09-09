02bb90545c77dc781bf5da26dc9c586bb96be42f
Yi Li <yi_li@ntu.edu.sg>
2022-09-09 20:45:40 +0800

    2 spaces

--- t3/q1/VendingMachine.java
+++ t3/q1/VendingMachine.java
@@ -1,17 +1,17 @@
 public class VendingMachine {
-    // constructor
-    public VendingMachine() {
-    }
+  // constructor
+  public VendingMachine() {
+  }
 
-    // get the drink selection, and return the cost of the drink
-    public double selectDrink() {...}
+  // get the drink selection, and return the cost of the drink
+  public double selectDrink() {...}
 
-    // insert the coins and returns the amount inserted
-    public double insertCoins(double drinkCost) {...}
+  // insert the coins and returns the amount inserted
+  public double insertCoins(double drinkCost) {...}
 
-    // check the change and print the change on screen
-    public void checkChange(double amount, double drinkCost)  {...}
+  // check the change and print the change on screen
+  public void checkChange(double amount, double drinkCost)  {...}
 
-    // print the receipt and collect the drink
-    public void printReceipt(){...}
+  // print the receipt and collect the drink
+  public void printReceipt(){...}
 }
